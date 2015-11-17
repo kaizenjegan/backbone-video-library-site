a06214214ebf13c8df825820e2474dc99e14dc4a
jegan_matthews <matthewsjegan@gmail.com>
2015-11-17 15:23:49 -0800

    minor refactoring

--- server.js
+++ server.js
@@ -22,8 +22,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({ secret: 'topsecret', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
