dd077d7172572077d5c8c07232a8d803f8fb857d
Jegan Matthews <matthewsjegan@gmail.com>
2016-12-29 23:39:13 -0800

    Updated twitter env config

--- env/config.js
+++ env/config.js
@@ -1,17 +1,17 @@
 var twitterStrategy = {
-	consumerKey: process.env.TwitterConsumerKey,
-	consumerSecret: process.env.TwitterConsumerSecret,
+	consumerKey: process.env.TwitterKey,
+	consumerSecret: process.env.TwitterSecret,
 	callback: process.env.TwitterCallback
 }
-  
+
 
 var config = {
 	twitter: twitterStrategy,
 	mongooseURL: process.env.MongooseURL,
 	isAuthenticated: function(req, res, next)
 	{
-		 if (req.isAuthenticated()) { 
-		 	return next(); 
+		 if (req.isAuthenticated()) {
+		 	return next();
 		 }
 		console.log('is not isAuthenticated');
 		res.status(401).send({'statusCode': '-1'})
